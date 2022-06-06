cccc16fe985252b192f3643efdec3267e124c3f5
Aviy-Coder <106950551+aviy-coder@users.noreply.github.com>
2022-06-06 12:42:32 +0530

    Update streamlit_app.py
    Added breakfast Menu header and Menu list

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,10 @@
 import streamlit
 
 streamlit.title('My parents new healthy dinner')
+
+streamlit.headr('Break Fast Menu')
+
+streamlit.text('Omega 3 & blueberry Oat meal')
+streamlit.text('Hard boiled egg')
+streamlit.text('Kale ,Spinech and Rocket Smoothie')
+streamlit.text('Banana Shake')
