c2c1ae8a726b0a18b0564bed7b752f3cf26764c3
Aviy-Coder <106950551+aviy-coder@users.noreply.github.com>
2022-06-06 14:59:51 +0530

    Update streamlit_app.py
    Added pick list

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,4 +14,8 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+my_fruit_list=my_fruit_list.set_index('Fruit')
+
+streamlit.multiselect("Pick Some Fruits :", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
