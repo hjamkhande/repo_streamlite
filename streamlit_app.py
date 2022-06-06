45b21fe3ee566a476bc56da7bcff0254ed04d492
Aviy-Coder <106950551+aviy-coder@users.noreply.github.com>
2022-06-06 12:47:55 +0530

    Update streamlit_app.py
    Added pics and updated menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,8 @@ import streamlit
 
 streamlit.title('My parents new healthy dinner')
 
-streamlit.header('Break Fast Menu')
-streamlit.text('Omega 3 & blueberry Oat meal')
-streamlit.text('Hard boiled egg')
-streamlit.text('Kale ,Spinech and Rocket Smoothie')
-streamlit.text('Banana Shake')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
