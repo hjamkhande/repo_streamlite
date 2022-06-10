da9a18e01a992a12b85c7a458844830296e8f434
Aviy-Coder <106950551+aviy-coder@users.noreply.github.com>
2022-06-10 18:02:01 +0530

    Update streamlit_app.py
    Added Header for fruitvicy_respons

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,8 @@ fruits_to_show=my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 #New Section to display Fruityvice api response
+streamlit.header('fruityvice Fruit Advice')
+
 import requests
 
 fruitvicy_response=requests.get("https://www.fruityvice.com/api/fruit/watermelon")
