8a9a5a1455df28025deeea0858176ae408a5dad0
Aviy-Coder <106950551+aviy-coder@users.noreply.github.com>
2022-06-15 23:18:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,6 +46,8 @@ streamlit.dataframe(fruitvicy_normalize)
 
 
 # Added Snowflake Connector
+import snowflake.connector
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
