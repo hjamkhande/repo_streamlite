c222a3069f3f840ba825ac0d1b37cbfbbce277b3
Aviy-Coder <106950551+aviy-coder@users.noreply.github.com>
2022-06-16 15:32:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,8 +58,8 @@ def init_connection():
 conn = init_connection()
 
 # Perform query.
-# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
-@st.experimental_memo(ttl=600)
+# Uses streamlit.experimental_memo to only rerun when the query changes or after 10 min.
+@streamlit.experimental_memo(ttl=600)
 def run_query(query):
     with conn.cursor() as cur:
         cur.execute(query)
