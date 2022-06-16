354ce87078afa210aac2e7887568bc05cf99bf37
Aviy-Coder <106950551+aviy-coder@users.noreply.github.com>
2022-06-16 16:32:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,10 +46,27 @@ streamlit.dataframe(fruitvicy_normalize)
 
 
 # Added Snowflake Connector
-import streamlit
-import sqlite3
+import streamlit as st
 import snowflake.connector
 
-my_cnx=snowflake.connector.connect(**streamlit.secrets["snowflake"]
-curr=my_cnx.cursor()                                                          
-curr.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+# Initialize connection.
+# Uses st.experimental_singleton to only run once.
+@st.experimental_singleton
+def init_connection():
+    return snowflake.connector.connect(**st.secrets["snowflake"])
+
+conn = init_connection()
+
+# Perform query.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+rows = run_query("SELECT * FROM FRUIT_LOAD_LIST;")
+
+# Print results.
+for row in rows:
+    st.write(f"{row[0]} has a :{row[1]}:")
